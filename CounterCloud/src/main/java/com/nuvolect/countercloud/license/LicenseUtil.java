fa4b713fade39417d693dd5bd5731f8128b105d9
Matt Kraus <matt.kraus@nuvolect.com>
2019-01-19 13:45:42 -0500

    Add copyright

--- CounterCloud/src/main/java/com/nuvolect/countercloud/license/LicenseUtil.java
+++ CounterCloud/src/main/java/com/nuvolect/countercloud/license/LicenseUtil.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019 Nuvolect LLC. 
+ * This software is offered for free under conditions of the GPLv3 open source software license. 
+ * Contact Nuvolect LLC for a less restrictive commercial license if you would like to use the software 
+ * without the GPLv3 restrictions.
+ */
+
 package com.nuvolect.countercloud.license;
 
 import android.content.Context;
